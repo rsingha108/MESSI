4c2bc2c13450107cd79f4756185c706db6545ca6
Rajdeep Mondal <mondalrajdeep814@gmail.com>
2024-04-14 19:02:49 -0700

    adding example for Batfish aggregation

---
+++ Mininet-Emulab/mininet/demo/Aggregation/agg.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pandas.io.formats.style import Styler
+from pybatfish.client.session import Session
+from pybatfish.datamodel import *
+from pybatfish.datamodel.answer import *
+from pybatfish.datamodel.flow import *
+from pybatfish.util import get_html
+from prefix_list_conversion import *
+
+bf = Session(host="localhost")
+
+NETWORK_NAME = 'example'
+SNAPSHOT_NAME = 'test_snapshot'
+
+SNAPSHOT_PATH = 'test_snapshot'
+
+bf.set_network(NETWORK_NAME)
+bf.init_snapshot(SNAPSHOT_PATH, name=SNAPSHOT_NAME, overwrite=True)
+
+result = bf.q.bgpRib(nodes='RouterD').answer().frame()
+
+res = result.values
+
+routes = []
+
+for row in res:
+    routes.append(row[2])
+    
+print(routes)
